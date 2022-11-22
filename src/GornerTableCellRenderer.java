50e1e7e2d8e4869c406ba681a3954e516f04349e
Nazar <nazarmozol@mail.ru>
2022-11-22 13:52:52 +0300

    v 2

--- src/GornerTableCellRenderer.java
+++ src/GornerTableCellRenderer.java
@@ -1,4 +1,5 @@
-package bsu.rfe.java.group7.lab3.Ivanov.varB4;
+
+
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.FlowLayout;
@@ -12,50 +13,58 @@ import javax.swing.table.TableCellRenderer;
 public class GornerTableCellRenderer implements TableCellRenderer {
     private JPanel panel = new JPanel();
     private JLabel label = new JLabel();
-    // Ищем ячейки, строковое представление которых совпадает с needle
-// (иголкой). Применяется аналогия поиска иголки в стоге сена, в роли
-// стога сена - таблица
     private String needle = null;
-    private DecimalFormat formatter =
-            (DecimalFormat)NumberFormat.getInstance();
+    private DecimalFormat formatter = (DecimalFormat)NumberFormat.getInstance();
+
     public GornerTableCellRenderer() {
-// Показывать только 5 знаков после запятой
         formatter.setMaximumFractionDigits(5);
-// Не использовать группировку (т.е. не отделять тысячи
-// ни запятыми, ни пробелами), т.е. показывать число как "1000",
-// а не "1 000" или "1,000"
         formatter.setGroupingUsed(false);
-// Установить в качестве разделителя дробной части точку, а не
-// запятую. По умолчанию, в региональных настройках
-// Россия/Беларусь дробная часть отделяется запятой
-        DecimalFormatSymbols dottedDouble =
-                formatter.getDecimalFormatSymbols();
+        DecimalFormatSymbols dottedDouble = formatter.getDecimalFormatSymbols();
         dottedDouble.setDecimalSeparator('.');
         formatter.setDecimalFormatSymbols(dottedDouble);
-// Разместить надпись внутри панели
+
         panel.add(label);
-// Установить выравнивание надписи по левому краю панели
         panel.setLayout(new FlowLayout(FlowLayout.LEFT));
+
     }
+
     public Component getTableCellRendererComponent(JTable table,
                                                    Object value, boolean isSelected, boolean hasFocus, int row, int col) {
-// Преобразовать double в строку с помощью форматировщика
+        if(col == 2){
+            label.setText(String.valueOf(value));
+            panel.setBackground(Color.white);
+            return panel;
+        }
         String formattedDouble = formatter.format(value);
-// Установить текст надписи равным строковому представлению числа
         label.setText(formattedDouble);
-        if (col==1 && needle!=null && needle.equals(formattedDouble)) {
-// Номер столбца = 1 (т.е. второй столбец) + иголка не null
-// (значит что-то ищем) +
-// значение иголки совпадает со значением ячейки таблицы -
-// окрасить задний фон панели в красный цвет
+
+
+
+
+        if (col == 1 && needle!=null && needle.equals(formattedDouble)) {
             panel.setBackground(Color.RED);
         } else {
-// Иначе - в обычный белый
-            panel.setBackground(Color.WHITE);
+
+            panel.setBackground(Color.white);
+
+            double num = Double.valueOf(label.getText());
+            int i = 0;
+            String str = String.valueOf(num);
+            while(i < str.length() && str.charAt(i++) != '.'){}
+            for(; i < str.length(); i++){
+                if(str.charAt(i) == '1' || str.charAt(i) == '3' || str.charAt(i) == '5'){
+                    panel.setBackground(Color.orange);
+                } else {
+                    panel.setBackground(Color.white);
+                    break;
+                }
+            }
         }
+
         return panel;
     }
     public void setNeedle(String needle) {
         this.needle = needle;
     }
 }
+
--- src/GornerTableModel.java
+++ src/GornerTableModel.java
@@ -1,12 +1,15 @@
+
+
 import javax.swing.table.AbstractTableModel;
+import java.math.BigInteger;
+
 @SuppressWarnings("serial")
 public class GornerTableModel extends AbstractTableModel {
     private Double[] coefficients;
     private Double from;
     private Double to;
     private Double step;
-    public GornerTableModel(Double from, Double to, Double step,
-                            Double[] coefficients) {
+    public GornerTableModel(Double from, Double to, Double step, Double[] coefficients) {
         this.from = from;
         this.to = to;
         this.step = step;
@@ -22,41 +25,55 @@ public class GornerTableModel extends AbstractTableModel {
         return step;
     }
     public int getColumnCount() {
-// В данной модели два столбца
-        return 2;
+        return 3;
     }
     public int getRowCount() {
-// Вычислить количество точек между началом и концом отрезка
-// исходя из шага табулирования
-        return new Double(Math.ceil((to-from)/step)).intValue()+1;
+        return Double.valueOf(Math.ceil((to-from)/step)).intValue()+1;
     }
+
     public Object getValueAt(int row, int col) {
-// Вычислить значение X как НАЧАЛО_ОТРЕЗКА + ШАГ*НОМЕР_СТРОКИ
-        double x = from + step*row;
-        if (col==0) {
-// Если запрашивается значение 1-го столбца, то это X
-            return x;
-        } else {
-// Если запрашивается значение 2-го столбца, то это значение
-// многочлена
-            Double result = 0.0;
-// Вычисление значения в точке по схеме Горнера.
-// Вспомнить 1-ый курс и реализовать
-            return result;
+        double x = from + step * row;
+        Double result = 0.0;
+        switch (col) {
+            case 0:
+                return x;
+            case 1: {
+                for (int i = coefficients.length - 1; i >= 0; i--) {
+                    result += coefficients[i] * Math.pow(x, coefficients.length - 1 - i);
+                }
+                return result;
+            }
+            case 2: {
+                for (int i = coefficients.length - 1; i >= 0; i--) {
+                    result += coefficients[i] * Math.pow(x, coefficients.length - 1 - i);
+                }
+                String str = String.valueOf(result);
+                String final_string = "";
+                int i = 0;
+                while(str.charAt(i) != '.'){
+                    final_string += str.charAt(i);
+                    i++;
+                }
+                BigInteger big = BigInteger.valueOf(Integer.valueOf(final_string));
+                boolean prime = big.isProbablePrime((int) Math.log(Integer.valueOf(final_string)));
+                return Boolean.valueOf(prime);
+            }
         }
+
+        //TODO: Realaze Gorgner scheam
+
+        return result;
     }
     public String getColumnName(int col) {
         switch (col) {
-            case 0:
-// Название 1-го столбца
-                return "Значение X";
-            default:
-// Название 2-го столбца
-                return "Значение многочлена";
+            case 0: return "Value of X";
+            case 1: return "Forward value";
+            case 2: return "Is [num] prime?";
         }
+        return "";
     }
     public Class<?> getColumnClass(int col) {
-// И в 1-ом и во 2-ом столбце находятся значения типа Double
         return Double.class;
     }
 }
+
